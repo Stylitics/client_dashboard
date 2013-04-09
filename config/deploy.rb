3e2cb7254d535204c0667c97e8ade0a166d72d1f
Cătălin Ilinca <catalin.ilinca@gmail.com>
2013-04-09 17:30:22 +0300

    Reverted to old and good deploy recipe. Will handle staging later.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,19 +1,8 @@
-# RVM bootstrap
-require 'capistrano/ext/multistage'
-require 'bundler/capistrano'
-require 'rvm/capistrano'
-
-set :rake, "/usr/local/bin/rake"
-set :rvm_bin_path , "/usr/local/rvm/bin"
-set :rvm_type, :system
-
-set :stages, %w(production staging)
-set :default_stage, 'staging'
-
 set :application, "dashboard"
 set :repository,  "git@github.com:Stylitics/client_dashboard.git"
 set :domain, "catalin@198.199.70.50"
 
+# set :deploy_to, "/apps/code/#{application}"
 set :deploy_to, "/apps/#{application}"
 
 set :scm, :git
@@ -21,9 +10,18 @@ set :scm, :git
 ssh_options[:paranoid] = false
 ssh_options[:forward_agent] = true
 
+# vps1-4 @ trisme
+# set :rake, "/usr/local/bin/rake"
+set :rake, "/usr/bin/rake"
+
 set :user, "catalin"
 set :runner, nil
 set :use_sudo, false
+
+role :web, domain
+role :app, domain
+role :db,  domain, :primary => true
+role :db,  domain
 
 namespace :deploy do
   task :start do ; end
@@ -40,14 +38,3 @@ end
 
 after "deploy", "create_assets_symlink"
 after "deploy:migrations", "create_assets_symlink"
-
-require 'bundler/capistrano'
-
-namespace :rvm do
-  desc 'Trust rvmrc file'
-  task :trust_rvmrc do
-    run "rvm rvmrc trust #{current_release}"
-  end
-end
-
-after "deploy:update_code", "rvm:trust_rvmrc"
--- config/deploy/staging.rb
+++
@@ -1,7 +0,0 @@
-set :rails_env, "production"
-set :rvm_ruby_string, 'ruby-2.0.0-p0'
-
-role :web, domain
-role :app, domain
-role :db,  domain, :primary => true
-role :db,  domain
