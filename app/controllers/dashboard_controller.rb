7bf68f99a63b41401ee4f16847c457c15d52a792
Cătălin Ilinca <catalin.ilinca@gmail.com>
2013-04-18 14:57:49 +0300

    Trend line filters

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,11 +8,31 @@ class DashboardController < ApplicationController
     @chart_run = @chart.last_run
     @chart_run.accessible = :all
     @chart_run[:gender] = "All"
+    @chart_run[:lo_age] = 10.to_s
+    @chart_run[:hi_age] = 100.to_s
     @chart_run[:student_opt] = "All"
     @chart_run[:location_opt] = "All"
+    @chart_run[:lo_price] = 0.to_s
+    @chart_run[:hi_price] = 100000.to_s
     @chart_run[:influencer_opt] = "Include"
     @chart_run[:staff_opt] = "Include"
-    @chart_run[:price] = "All"
+    @chart_run[:style_opt] = "Include"
+    @chart_run[:event_type] = "All"
+    @chart_run[:color_opt] = "Include"
+    @chart_run[:retailer_opt] = "Include"
+    @chart_run[:brand_opt] = "All"
+    @chart_run[:pattern_opt] = "All"
+    @chart_run[:fabric_opt] = "All"
+    @chart_run[:sort_opt] = "User ID"
+    @chart_run[:style_txt] = ""
+    @chart_run[:color_txt] = ""
+    @chart_run[:brand_txt] = ""
+    @chart_run[:retailer_txt] = ""
+    @chart_run[:pattern_txt] = ""
+    @chart_run[:fabric_txt] = ""
+
+    @chart_run[:start_date_txt] = Time.now.strftime("%Y-%m-%d")
+    @chart_run[:end_date_txt] = 6.months.ago.strftime("%Y-%m-%d")
     @chart.runs << @chart_run
   end
 end
