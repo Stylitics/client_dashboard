3f280082c6554137e5940f82397a13dc3d09b009
Cătălin Ilinca <catalin.ilinca@gmail.com>
2013-05-27 15:15:57 +0300

    add collections for inputs

--- app/controllers/chart_runs_controller.rb
+++ app/controllers/chart_runs_controller.rb
@@ -10,16 +10,36 @@ class ChartRunsController < ApplicationController
 
     # this is ugly at this moment. will refactor better later
 
-    case chart.slug
-    when "trend-line"
-      [:location_opt, :style_opt, :retailer_opt, :brand_opt].each do |p|
-        params[:chart_run][p] = params[:chart_run][p].reject{|l| l.blank?}.join(",") if params[:chart_run][p].present?
-      end
-
-      [:brand_search, :color_search, :event_type_search, :fabric_search, :pattern_search, :retailer_search, :style_search].each do |s|
-        params[:chart_run][s] = params[:chart_run][s].reject{|l| l.blank?}
-      end
+    [
+      :location_opt,
+      :brand_add_opt,
+      :brand_sub_opt,
+      :retailer_add_opt,
+      :retailer_sub_opt,
+      :style_add_opt,
+      :style_sub_opt,
+      :color_add_opt,
+      :color_sub_opt,
+      :pattern_add_opt,
+      :pattern_sub_opt,
+      :fabric_add_opt,
+      :fabric_sub_opt,
+      :brand_add_search,
+      :brand_sub_search,
+      :retailer_add_search,
+      :retailer_sub_search,
+      :style_add_search,
+      :style_sub_search,
+      :color_add_search,
+      :color_sub_search,
+      :pattern_add_search,
+      :pattern_sub_search,
+      :fabric_add_search,
+      :fabric_sub_search
+    ].each do |p|
+      params[:chart_run][p] = params[:chart_run][p].reject{|l| l.blank?}.join(",") if params[:chart_run][p].present?
     end
+
     chart_run.update_attributes params[:chart_run]
 
     TrendLineWorker.perform_async(chart_run.id)
