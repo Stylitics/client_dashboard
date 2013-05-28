d9ae462ed9b24893abdbca2019f97ec78884f913
Cătălin Ilinca <catalin.ilinca@gmail.com>
2013-05-28 16:21:56 +0300

    Filters in place

--- app/workers/trend_line_worker.rb
+++ app/workers/trend_line_worker.rb
@@ -19,11 +19,12 @@ class TrendLineWorker
     # end SQL connection
 
     code.scan(/\{\{(.*?)\}\}/).each do |v|
-      begin
-        code.gsub!("{{#{v[0].camelize(:lower)}}}", chart_run[v[0].camelize(:lower).underscore.to_sym])
-      rescue
-        logger.info v[0]
-      end
+      code.gsub!("{{#{v[0].camelize(:lower)}}}", chart_run[v[0].camelize(:lower).underscore.to_sym].to_a.join(","))
+      # begin
+      #   code.gsub!("{{#{v[0].camelize(:lower)}}}", chart_run[v[0].camelize(:lower).underscore.to_sym])
+      # rescue
+      #   logger.info v[0]
+      # end
     end
 
     code.gsub!('{#json_output#}', "#{Rails.root}/tmp/runs/#{chart_run.id}.json")
