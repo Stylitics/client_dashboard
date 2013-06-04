623bb06cbf414767d1f9237df18ba7a101d4824b
Cătălin Ilinca <catalin.ilinca@gmail.com>
2013-06-04 23:37:51 +0300

    no more workers for scripts.

--- app/controllers/chart_runs_controller.rb
+++ app/controllers/chart_runs_controller.rb
@@ -43,13 +43,57 @@ class ChartRunsController < ApplicationController
     end
 
     chart_run.update_attributes params[:chart_run]
-    chart.update_attribute :is_running, true
+    # chart.update_attribute :is_running, true
 
-    TrendLineWorker.perform_async(chart_run.id)
+    # chart_run = ChartRun.find(chart_run_id)
+    # chart = chart_run.chart
 
-    respond_to do |format|
-      format.js
+    code = chart.r_script.code
+
+    # this is dupplicate. will refactor later on
+
+    # SQL connection
+    code.gsub!("{!hostVar!}", SQL_HOST)
+    code.gsub!("{!portVar!}", SQL_PORT.to_s)
+    code.gsub!("{!dbnameVar!}", SQL_DB)
+    code.gsub!("{!userVar!}", SQL_USER)
+    code.gsub!("{!passVar!}", SQL_PASS)
+    # end SQL connection
+
+    code.scan(/\{\{(.*?)\}\}/).each do |v|
+      injected_value = chart_run[v[0].camelize(:lower).underscore.to_sym].to_a.join(",")
+      injected_value = "NULL" if injected_value.blank?
+      code.gsub!("{{#{v[0].camelize(:lower)}}}", injected_value)
     end
+    # remove "NULL"s
+    code.gsub!('"NULL"', "NULL")
+
+    code.gsub!('{#json_output#}', "#{Rails.root}/tmp/runs/#{chart_run.id}.json")
+    File.open("#{Rails.root}/tmp/runs/#{chart_run.id}.r", 'w') {|f| f.write(code) }
+
+    e = system "Rscript '#{Rails.root}/tmp/runs/#{chart_run.id}.r'"
+
+    json_output = ''
+    if File.exists?("#{Rails.root}/tmp/runs/#{chart_run.id}.json")
+      f = File.new("#{Rails.root}/tmp/runs/#{chart_run.id}.json", "r")
+      while (l = f.gets)
+        json_output << l
+      end
+      f.close
+    end
+
+    chart_run.output = JSON.pretty_generate(JSON.parse(json_output))
+    chart_run.save
+
+    redirect_to trends_path
+
+    # chart.update_attribute :is_running, false
+
+    # TrendLineWorker.perform_async(chart_run.id)
+
+    # respond_to do |format|
+    #   format.js
+    # end
   end
 
   def check
