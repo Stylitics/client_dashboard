b87ac9c99bb961afeadb66e2ae415e96f123c0b1
Cătălin Ilinca <catalin.ilinca@gmail.com>
2013-06-17 16:31:17 +0300

    Added dynamic output to chart.

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,22 +1,25 @@
 class ChartsController < ApplicationController
   def show
-    if params[:id] == "top-25-brands-and-retailers"
-      temp = "temp"
-    elsif params[:id] == "top-10"
-      temp = "temp2"
-    elsif params[:id] == "trend-line"
-      temp = "temp3"
-    elsif params[:id] == "brand-share"
-      temp = "temp3"
+    @chart = Chart.find(params[:id])
+    if @chart.r_script.running_code.present?
+      if @chart.last_run.output.present?
+        render json: @chart.last_run.output
+      else
+        render json: '{"data": "empty"}'
+      end
     else
-      temp = "temp4"
+      if params[:id] == "top-25-brands-and-retailers"
+        temp = "temp"
+      elsif params[:id] == "top-10"
+        temp = "temp2"
+      elsif params[:id] == "trend-line"
+        temp = "temp3"
+      elsif params[:id] == "brand-share"
+        temp = "temp3"
+      elsif params[:id] == "weather"
+        temp = "temp4"
+      end
+      render file: "#{Rails.root}/app/assets/javascripts/#{temp}.json"
     end
-    render file: "#{Rails.root}/app/assets/javascripts/#{temp}.json"
-    # @chart = Chart.find(params[:id])
-    # if @chart.last_run.output.present?
-    #   render json: @chart.last_run.output
-    # else
-    #   render json: '{"data": "empty"}'
-    # end
   end
 end
